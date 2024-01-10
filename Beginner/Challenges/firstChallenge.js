f9c8b4c32fe83f980092c300dc812aeb70e53d0d
Arvie Regato <arvie.regato@gmail.com>
2024-01-10 09:44:08 +0800

    Create Notes for beginner Phase. About Comments , Variables and Console logs. First Challenge

---
+++ Beginner/Challenges/firstChallenge.js
@@ -0,0 +1,16 @@
+// 1. Create variable name (name) & store your name
+let name = "Arvie";
+
+// 2. Create variable name (WhatDoYouWannaBecomeInYourLife) & store your "programmer"
+let WhatDoYouWannaBecomeInYourLife = "Software Engineer";
+
+// 3. Create variable name (gender) & store your gender
+let gender = "Male";
+
+// 4. Create variable name (email) & store your email
+let email = "arvie.regato@gmail.com";
+
+console.log("Student Name : " + name);
+console.log("Profession I want to became : " + WhatDoYouWannaBecomeInYourLife);
+console.log("Student Gender : " + gender);
+console.log("Email : " + email);
---
+++ Beginner/index.js
@@ -0,0 +1,7 @@
+// Show table in console using objects
+console.table({ name: "Arvie", age: "27" });
+
+// Assigning of Variable
+let fav_fruit = "Orange";
+fav_fruit = "Dalandan";
+console.log(fav_fruit);
